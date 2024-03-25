fa72db915a2e6056fecaee5e090633a3642a29ea
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-03-24 22:03:04 -0700

    Publish

---
+++ main.js
@@ -0,0 +1,8 @@
+//Question 49:
+function loghobies(...hobies) {
+    hobies.forEach(hobies => {
+        console.log(`I enjoy ${hobies}`);
+    });
+}
+loghobies("reading", "coding", "travelling");
+export {};
---
+++ main.ts
@@ -0,0 +1,14 @@
+//Question 49:
+function loghobies(...hobies: string[]){
+    hobies.forEach(hobies=>{
+        console.log(`I enjoy ${hobies}`)
+    })
+}
+loghobies("reading", "coding", "travelling")
+
+//Question 50:
+let mayday = `My ideal day involves:
+1.Wake up early
+2.Exersize daily
+3.Code daily`
+console.log(mayday)
